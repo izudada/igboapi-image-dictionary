26b9b334b664031c2aeffa9549c9a42ab8880e04
Anthony <tonyudeagbala@gmail.com>
2021-12-29 20:43:45 +0000

    Add comments

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+# iNITIALIZING IGBO API AND PIXABAY API KEYS
 igbo_api_key_value = os.environ.get("IGBO_API_KEY")
 igbo_api_key = {'X-API-Key': igbo_api_key_value}
 pixabay_api_key = os.environ.get("PIXABAY_API_KEY")
@@ -13,7 +13,16 @@ pixabay_api_key = os.environ.get("PIXABAY_API_KEY")
 
 @app.route("/api/v1/<string:igbo_word>")
 def index(igbo_word):
+
+    """
+    iA single endpoint that queries Igbo API endpoint for an igbo_word,
+    gets various definitions of that word, sends each derivate through
+    pixabay API with the end result of getting corresponding images to
+    """
+
     images = []
+
+    # Hitting Igbo API endpoint
     response = requests.get(f"https://igboapi.com/api/v1/words?keyword={igbo_word}", headers=igbo_api_key)
     result = response.json()
     
@@ -31,5 +40,5 @@ def index(igbo_word):
 
 
 if __name__ == '__main__':
-    app.secret_key = 'secret@'
+    app.secret_key = os.environ.get("APP_SECRET")
     app.run(debug=True)
